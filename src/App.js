76492003f48eadce3908bfda14c9680b19eb6957
user <user@MacBook-Pro-user.local>
2023-09-24 17:33:01 +0300

    Add tweet current quote on Twitter functionality

--- src/App.js
+++ src/App.js
@@ -32,9 +32,18 @@ function App() {
     return Math.round(Math.random() * number)
   }
 
-  function handleClick() {
+  // Handle button clicks
+  function handleQuote() {
     getData()
   }
+
+  // async function handleTweet() {
+  //   try {
+  //     alert('ok')
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
 
   return (
     <div id="quote-box" className="w-96 h-72 bg-[#F8F8FF] p-4 rounded-lg">
@@ -46,23 +55,32 @@ function App() {
       </div>
 
       <div className="grid grid-flow-col gap-52 h-1/6">
-        <button
-          id="tweet-quote"
-          class="bg-gray-500 opacity-80 hover:opacity-100 text-white font-bold py-2 px-4 rounded w-14 grid place-items-center"
+        <a
+          // onClick={handleTweet}
+          // href="https://twitter.com/intent/tweet"
+          href={`https://twitter.com/intent/tweet?hashtags=quotes&text="${encodeURIComponent(
+            quote.text
+          )}" - ${encodeURIComponent(quote.author)}`}
+          target="_top"
+          title="Tweet this quote!"
+          className="bg-gray-500 opacity-80 hover:opacity-100 text-white font-bold py-2 px-4 rounded w-14 grid place-items-center"
         >
-          {/* Twitter Icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-          </svg>
-        </button>
+          <button id="tweet-quote">
+            {/* Twitter Icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+            </svg>
+          </button>
+        </a>
         <button
-          onClick={handleClick}
+          onClick={handleQuote}
           id="new-quote"
+          title="More quotes!"
           className="bg-gray-500 opacity-80 hover:opacity-100 text-white font-bold py-2 px-4 rounded w-20"
         >
           More!
